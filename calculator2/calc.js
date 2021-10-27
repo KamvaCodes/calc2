efc0db6acd37ded8b22d57b5912ce65368dd33e4
Kamva <fatmankamvelihle@gmail.com>
2021-10-27 10:08:15 +0200

    updates

--- calculator2/calc.js
+++ calculator2/calc.js
@@ -1,19 +1,19 @@
 
-//save all the buttons inside the buttons variable
+//save all the buttons inside a variable
 let buttons = document.querySelectorAll('button')
 
 //save the buttons inside the
-const displayOutput = document.querySelector('.display')
+const displayOutput = document.getElementById("display");
 
 buttons.forEach(function(button) {
     button.addEventListener('click', calculate)
   });
 
 // calculate function
-function calculate(event) {
+function calculate(button) {
     // current clicked buttons value
 
-    const clickedButton = event.target.value;
+    const clickedButton = button.target.value;
   
     if (clickedButton === '=') {
       // check if the display is not empty then only do the calculation
